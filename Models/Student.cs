c9a8d996c2ea230e8e39e5f7581b2dda40d830ce
Michael Henderson <mvhenderson.4@icloud.com>
2017-09-22 10:05:59 -0500

    add string length attributes to frstname and lastname of student model

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,7 +7,11 @@ namespace mentorient.Models
     public class Student
     {
         public int Id { get; set; } 
+
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstName { get; set; }
 
         [DataType(DataType.Date)]
