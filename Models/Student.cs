2a857704a046a2353d80ea7300f74d0d31cbd848
Michael Henderson <mvhenderson.4@icloud.com>
2017-09-22 09:27:35 -0500

    create seed data for database

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,7 +7,7 @@ namespace mentorient.Models
     {
         public int Id { get; set; } 
         public string LastName { get; set; }
-        public string FristName { get; set; }
+        public string FirstName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments {get; set;}
