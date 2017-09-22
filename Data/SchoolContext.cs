8bc585f165f18f73514c13ea2dd2c09d5cc0baba
Michael Henderson <mvhenderson.4@icloud.com>
2017-09-22 09:15:20 -0500

    Create db context

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,17 @@
+using mentorient.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mentorient.Data
+{   
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+
+}
